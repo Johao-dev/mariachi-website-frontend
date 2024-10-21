0ed452194a5555e5c9c024796f9470b4d994fd4b
johaoDev <johaobravoespinoza1501@gmail.com>
2024-10-20 19:15:02 -0500

    feature-index-page 19:14 20/10/24 semi-finished home page

---
+++ js/controllers/indexController.js
@@ -0,0 +1,35 @@
+
+
+
+/*----  <  COMMENTS SLIDER  >  ----*/
+
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dinamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // Responsive breakpoints
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      }
+    }
+  });
